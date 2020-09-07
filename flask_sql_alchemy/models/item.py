2f0ef62587e980bda74be2830757827f508bdf83
pavan-elisetty <57026704+pavan-elisetty@users.noreply.github.com>
2020-09-07 15:15:51 +0530

    Add files via upload

--- flask_sql_alchemy/models/item.py
+++ flask_sql_alchemy/models/item.py
@@ -20,34 +20,14 @@ class ItemModel(db.Model):
         return {'name':self.name , 'price':self.price}
 
     @classmethod
-    def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return cls(*row)
+    def find_by_name(cls, name): #cls==ItemModel
+        return cls.query.filter_by(name=name).first() #SELECT * FROM items WHERE name=name LIMIT 1
 
 
-    def insert(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+    def save_to_db(self):
+        db.session.add(self)   #this method is useful to both update and insert
+        db.session.commit()
 
-        query = "INSERT INTO items VALUES (?,?)"
-        cursor.execute(query, (self.name,self.price))
-        connection.commit()
-        connection.close()
-
-
-    def update(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query, (self.price,self.name))
-        connection.commit()
-        connection.close()
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
