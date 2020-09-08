273214321d0e0cb6eec16e93ad64baf645dfee15
pavan-elisetty <57026704+pavan-elisetty@users.noreply.github.com>
2020-09-08 19:55:32 +0530

    Add files via upload

--- flask_sql_alchemy/models/store.py
+++ flask_sql_alchemy/models/store.py
@@ -1,24 +1,19 @@
-#models are sumtng which works internally , used for internal representation
 from db import db
 
 
-class ItemModel(db.Model):
+class StoreModel(db.Model):
     __tablename__ = 'items'
     id = db.Column(db.Integer , primary_key = True)
     name=db.Column(db.String(80))
-    price = db.Column(db.Float(precision = 2))
-    store_id = db.Column(db.Integer , db.ForeignKey('stores.id'))
-    store = db.relationship('StoreModel')
 
+    items = db.relationship('ItemModel' , lazy = 'dynamic')
 
-    def __init__(self,name,,store_id):
+    def __init__(self,name):
         self.name = name
-        self.price = price
-        self.store_id=store_id
 
     def json(self):
-        return {'name':self.name , 'price':self.price}
-
+        return {'name':self.name , 'items':[item.json() for item in self.items.all()]}
+                                                                          #acting as query builder
     @classmethod
     def find_by_name(cls, name): #cls==ItemModel
         return cls.query.filter_by(name=name).first() #SELECT * FROM items WHERE name=name LIMIT 1
